c6153cf89789f5cab3a2bd6e81f2580f63af345c
l44x <138518422+claudiosc01@users.noreply.github.com>
2024-07-16 20:22:10 -0500

    Add files via upload

---
+++ bash_banner.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+#Colours
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+whiteColour="\e[1;37m"
+
+figlet -f slant -c "| GoodR00t" | lolcat --force
+echo -e "\t\t\t\t    ${redColour}███${endColour}${whiteColour}███${endColour}${redColour}███${endColour}\n\n\n" 
